4bff1138bffe67ab160a02cdd20c3f22a568b843
Tawana <tawanakwaramba@gmail.com>
2020-10-18 10:20:59 +0800

    initial commit

---
+++ programme/task.sh
@@ -0,0 +1,9 @@
+#retriving all the trainning images from the absolute path of
+#home/student/train
+
+#retriving all the validation images from the absolute path of
+#home/student/val
+
+#retriving all the testing images from the abosolute path of
+#home/student/test
+echo 'hello world'
