52e42b352f6664a95c663034a2bfb08246a06def
Tawana <tawanakwaramba@gmail.com>
2020-10-20 03:02:57 +0800

    still trying to figure out how to extract the area of interest

--- programme/playGround.py
+++ programme/playGround.py
@@ -3,45 +3,50 @@ import numpy as np
 import os
 
 all_imgs = os.listdir('../train_updated/')
+print(all_imgs)
+interest = ["tr17.jpg"]
 
-im = cv.imread('../train_updated/tr01.jpg')
+for ii, path in  enumerate(interest):
+    im = cv.imread('../train_updated/' + path)
 
-gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-gray = cv.GaussianBlur(gray, (5,5), 0)
-cv.imshow("blurred image", gray)
-thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
+    gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+    gray = cv.GaussianBlur(gray, (5,5), 0)
+    cv.imshow("blurred image %s" %ii, gray)
+    thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
 
-cv.imshow("threshold image", thresh)
+    cv.imshow("threshold image %s" %ii, thresh)
 
-edge_thresh = 100
-canny_trans = cv.Canny(thresh, edge_thresh, edge_thresh*2 )
-cv.imshow("canny_trans before", canny_trans)
+    edge_thresh = 100
+    canny_trans = cv.Canny(thresh, edge_thresh, edge_thresh*2 )
+    cv.imshow("canny_trans before %s" %ii, canny_trans)
 
-rect_kern = cv.getStructuringElement(cv.MORPH_RECT, (3,3))
-canny_trans = cv.erode(canny_trans, None, iterations=1)
-canny_trans = cv.dilate(thresh, None, iterations=1)
-cv.imshow("found edges after morphology", canny_trans)
+    rect_kern = cv.getStructuringElement(cv.MORPH_RECT, (5,5))
+    canny_trans = cv.erode(canny_trans, None, iterations=1)
+    canny_trans = cv.dilate(thresh, None, iterations=1)
+    cv.imshow("found edges after morphology %s" %ii, canny_trans)
+#
+#    cnts, heirarchy = cv.findContours(canny_trans, cv.RETR_TREE,
+#            cv.CHAIN_APPROX_SIMPLE)[1:]
+#
+#    copy = im.copy()
+#    copy2 = im.copy()
+#    cv.drawContours(copy, cnts, -1, (0,255,0), 3)
+#    cv.imshow("all contorus %s" %ii, copy)
+#
+#    cnts = sorted(cnts, key =cv.contourArea, reverse = True )
+#    total_cnts = len(cnts)
+#    keep = total_cnts * 0.7
+#    keep = int(keep)
+#
+#    #the contours of the actual numbers is not going to be the biggest or the
+#    #smallest, so I am going to get rid of half of the contours mainly
+#    #focusing on the smaller contour
+#    cnts = cnts[:keep]
+#    print(len(cnts))
+#    cv.drawContours(copy2, cnts, -1, (0,0,255), 2)
+#    cv.imshow("image after filtered contours %s" %ii, copy2)
 
-cnts, heirarchy = cv.findContours(canny_trans, cv.RETR_TREE,
-        cv.CHAIN_APPROX_SIMPLE)[1:]
 
-copy = im.copy()
-copy2 = im.copy()
-cv.drawContours(copy, cnts, -1, (0,255,0), 3)
-cv.imshow("all contorus", copy)
-
-cnts = sorted(cnts, key =cv.contourArea, reverse = True )
-print(len(cnts))
-total_cnts = len(cnts)
-keep = total_cnts // 2
-
-#the contours of the actual numbers is not going to be the biggest or the
-#smallest, so I am going to get rid of half of the contours mainly
-#focusing on the smaller contour
-cnts = cnts[:keep]
-print(len(cnts))
-cv.drawContours(copy2, cnts, -1, (0,0,255), 2)
-cv.imshow("image after filtered contours", copy2)
 cv.waitKey()
 cv.destroyAllWindows()
 
